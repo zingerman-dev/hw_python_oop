c199e3abb356cb5cdf4f7122c9dc21d1d10e5675
Peter Zingerman <zingerman.dev@gmail.com>
2022-10-13 19:02:01 +0300

    Переписано в соответствии с новым ТЗ

--- homework.py
+++ homework.py
@@ -26,6 +26,7 @@ class Training:
     """Базовый класс тренировки."""
     LEN_STEP: float = 0.65  # длина шага по умолчанию
     M_IN_KM: int = 1000  # метров в километре
+    MIN_IN_H: int = 60  # минут в часе
 
     def __init__(self,
                  action: int,  # количество действий (шагов, грибков и тд)
@@ -75,20 +76,23 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
+    CALORIES_MEAN_SPEED_MULTIPLIER: int = 18  # коэф. для расчета калорий 1
+    CALORIES_MEAN_SPEED_SHIFT: int = 1.79  # коэф. для расчета калорий 2
+
     def get_spent_calories(self) -> float:
         """
         Получить количество затраченных калорий.
         Вычисляет затраченные калории по формуле:
-        (18 * средняя_скорость - 20) * вес / 1000 * время_тренировки
+        (18 * средняя_скорость + 1,79) * вес / 1000 * время_тренировки
         """
-        M_IN_KM: int = 1000  # метров в километре
-        C_COEF1: int = 18  # коэф. для расчета калорий при беге 1
-        C_COEF2: int = 20  # коэф. для расчета калорий при беге 2
         speed: float = self.get_mean_speed()  # средняя скорость
         weight: float = self.weight  # вес
         dur: float = self.duration  # продолжительность тренировки
-        calories: float = (C_COEF1 * speed - C_COEF2) * weight / M_IN_KM * dur
-        return calories
+        calories: float = self.CALORIES_MEAN_SPEED_MULTIPLIER * speed
+        calories = calories + self.CALORIES_MEAN_SPEED_SHIFT
+        calories = calories * weight / self.M_IN_KM * dur
+        return calories  # Из 1й строки формулы полотно-портянка кода
+        # ОБОЖАЮ ПЕП8. Все для читаемости кода, ведь так понятне, конечно!
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке.
@@ -106,13 +110,19 @@ class Running(Training):
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    LEN_STEP: float = 0.64  # длина гребка
+    LEN_STEP: float = 0.64  # длина шага
+    CALORIES_WEIGHT_MULTIPLIER: int = 0.035  # коэф. для расчета калорий 1
+    CALORIES_MEAN_SPEED_MULTIPLIER: int = 2  # коэф. для расчета калорий 2
+    CALORIES_SPEED_HEIGHT_MULTIPLIER: int = 0.029  # коэф. для расчета калорий3
+    KMH_IN_MSEC: int = 0.278  # км/ч в м/с
+    CM_IN_M: int = 100  # сантиметров в метре
+    S_IN_M: int = 60  # сек в минуте
 
     def __init__(self,
                  action: int,  # количество действий (шагов, грибков и тд)
-                 duration: float,  # продолжительность тренировки
-                 weight: float,  # вес тренирующегося
-                 height: float  # высота тренирующегося
+                 duration: float,  # продолжительность тренировки В Ч
+                 weight: float,  # вес тренирующегося В КГ
+                 height: float  # высота тренирующегося В СМ
                  ) -> None:
         super().__init__(action, duration, weight)
         self.height = height
@@ -123,15 +133,15 @@ class SportsWalking(Training):
         Вычисляет затраченные калории по формуле:
         (0.035 * вес + (скорость * 2 / рост) * 0.029 * вес) * время_тренировки
         """
-        C_COEF_1: int = 0.035  # коэф. для расчета калорий 1
-        C_COEF_2: int = 2  # коэф. для расчета калорий 2
-        C_COEF_3: int = 0.029  # коэф. для расчета калорий 3
-        wi: float = self.weight
-        hi: float = self.height
-        sp: float = self.get_mean_speed()
-        dur: float = self.duration
-        calories: float = 0
-        calories = (C_COEF_1 * wi + (sp * C_COEF_2 / hi) * C_COEF_3 * wi) * dur
+        weight: float = self.weight  # вес
+        height: float = self.height / self.CM_IN_M  # рост
+        speed: float = self.get_mean_speed() * self.KMH_IN_MSEC  # скорость
+        duration: float = self.duration / self.MIN_IN_H / self.S_IN_M
+        calories: float = 0  # калории
+        calories = speed * self.CALORIES_MEAN_SPEED_MULTIPLIER / height
+        calories *= self.CALORIES_SPEED_HEIGHT_MULTIPLIER * weight
+        calories += self.CALORIES_WEIGHT_MULTIPLIER * weight
+        calories *= duration
         return calories
 
     def show_training_info(self) -> InfoMessage:
@@ -151,6 +161,8 @@ class SportsWalking(Training):
 class Swimming(Training):
     """Тренировка: плавание."""
     LEN_STEP: float = 1.38  # длина гребка
+    CALORIES_MEAN_SPEED_SHIFT: int = 1.1  # слагаемое скорости
+    CALORIES_WEIGHT_MULTIPLIER: int = 2  # множитель для расчета калорий
 
     def __init__(self,
                  action: int,  # количество действий (шагов, грибков и тд)
@@ -169,9 +181,9 @@ class Swimming(Training):
         длина_бассейна * количество_бассейнов / 1000 / время_тренировки
         (1000 - метров в км)
         """
-        M_IN_KM: int = 1000  # метров в километре
-        speed: float = 0  # тип отдельно, чтобы влезть в 79 символов PEP8
-        speed = self.length_pool * self.count_pool / M_IN_KM / self.duration
+        speed: float = 0
+        speed = self.length_pool * self.count_pool / self.M_IN_KM
+        speed /= self.duration
         return speed
 
     def get_spent_calories(self) -> float:
@@ -180,11 +192,13 @@ class Swimming(Training):
         Вычисляет затраченные калории по формуле:
         (средняя_скорость + 1.1) * 2 * вес
         """
-        C_COEF1: int = 1.1  # коэф. для расчета калорий при плавании 1
-        C_COEF2: int = 2  # коэф. для расчета калорий при плавании 2
+        CALORIES_MEAN_SPEED_SHIFT: int = 1.1  # слагаемое скорости
+        CALORIES_WEIGHT_MULTIPLIER: int = 2  # множитель для расчета калорий
         speed: float = self.get_mean_speed()  # скорость
         weight: float = self.weight  # вес
-        calories: float = (speed + C_COEF1) * C_COEF2 * weight
+        duration: float = self.duration
+        calories: float = speed + CALORIES_MEAN_SPEED_SHIFT
+        calories *= CALORIES_WEIGHT_MULTIPLIER * weight * duration
         return calories
 
     def show_training_info(self) -> InfoMessage:
